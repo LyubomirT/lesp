c751687c16565ba94d0577f9a3bda44e93235bd3
LyubomirT <terlyubomir@gmail.com>
2023-11-26 15:50:44 +0200

    Improve demo.py, add demo configuration

--- demo.py
+++ demo.py
@@ -1,4 +1,13 @@
-from main import is_correct, get_similar
+from ac.autocorrect import is_correct, get_similar
+
+def load_config():
+    with open("demo_config", "r") as f:
+        config = f.read()
+    return config
+
+config = load_config()
+# showallsimilarities="True"
+showallsimilarities = config.split("showallsimilarities=\"")[1].split("\"")[0]
 
 def demo():
     while True:
@@ -7,9 +16,13 @@ def demo():
             print("Correct!")
         else:
             print("Incorrect!")
-            similar = get_similar(word, 0.7, chunks=20, upto=5)
+            similar = get_similar(word, 0.5, chunks=20, upto=5)
             if similar == None:
                 print("No similar words found.")
+            elif showallsimilarities == "True":
+                print("Did you mean any of the following?")
+                for w in similar:
+                    print("\"" + w + "\"")
             else:
                 similar = similar[0]
                 print("Did you mean \"" + similar + "\"?")
